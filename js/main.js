bfa48129535136092cb75ca2aa3a612df3f9e160
Fisayo Watti <wf.watti@gmail.com>
2021-01-26 14:49:37 +0100

    fade out header image on scoll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-function init(){
-    
+// gsap.registerPlugin(ScrollTrigger);
+
+function init() {
+  gsap.to("#intro img", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: "#intro",
+      start: "top top",
+      end: "bottom center",
+      scrub: true,
+      markers: true,
+    },
+  });
 }
 
-window.addEventListener('load', function(){
-    init();
+window.addEventListener("load", function () {
+  init();
 });
